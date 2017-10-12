68810b71fd27e389b2b9f798ffb27eb9d8ab0152
William Leingang <wleingang15@mail.wou.edu>
2017-10-12 15:00:42 -0700

    finishing up the javascript and a bit more formatting.

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 var storedType = 'Concept';
 
 function setText(type) {
-    if(storedType != type){
+    if (storedType != type) {
+        $('#info').remove();
         document.getElementById('Title').innerHTML = type + " Generator";
         storedType = type
         document.getElementById('GenerationField').innerHTML = ""
+    }
+    if (type == "Chain") {
+        $('#info').remove();
+        $(".buttonRow").append("<input type='text', name='Enter Number', id='info', value='Input number of sentences'>");
+    }
+    else {
+        $('#info').remove();
     }
 }
 
@@ -15,6 +23,16 @@ function generatePrompt() {
     else if (storedType == 'Sentence') {
         document.getElementById('GenerationField').innerHTML = storedType + " Prompt Placeholder."
     }
+    else if (storedType == 'Chain') {
+        var i2 = document.getElementById("info").value;
+        $('.chainList').remove();
+        $(".GenerationField").append("<ol, class='chainList'></ol>");
+        var i = 0;
+        while(i < i2 && i < 5){
+            $(".chainList").append("<li>Chain Prompt Placeholder.</li>");
+            i++;
+        }
+    }
     else if (storedType == 'Setting') {
         document.getElementById('GenerationField').innerHTML = storedType + " Prompt Placeholder."
     }
@@ -22,7 +40,8 @@ function generatePrompt() {
         document.getElementById('GenerationField').innerHTML = storedType + " Prompt Placeholder."
     }
     else {
-        document.getElementById('GenerationField').innerHTML = storedType + " Prompt Placeholder."
+        $('.Character').remove();
+        $(".GenerationField").append("<table style='width:100%', class='Character'><tr><th>First</th><th>Lastname</th><th>Age</th><th>Profession</th></tr ><tr><td>NA</td><td>NA</td><td>NA</td><td>NA</td></tr></table>");
     }
 }
 
