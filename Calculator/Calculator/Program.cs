132716b16af5149ebb656a3db5ae2d19b133e283
William Leingang <wleingang15@mail.wou.edu>
2017-10-17 16:43:44 -0700

    Created Main method, hopefully converted correctly, starting to convert DoCalculation()

--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -6,10 +6,26 @@ using System.Threading.Tasks;
 
 namespace Calculator
 {
-    class Program
+    class Calculator
     {
+        private IStackADT stack = new LinkedStack();
         static void Main(string[] args)
         {
+            Calculator app = new Calculator();
+            bool playAgain = true;
+            Console.WriteLine("\nPostfix Calculator. Recognizes these operators: + - * /");
+            while(playagain)
+            {
+                playAgain = app.DoCalculation();
+            }
+            Console.WriteLine("Bye.");
+        }
+
+        bool DoCalculation()
+        {
+            Console.WriteLine("Please enter Q to quit\n");
+            string input = "2 2 + ";
+            Console.WriteLine("> ");
         }
     }
 }
